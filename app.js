cf8b88d7b17ca1146beab174b6394840d240766c
fstipo <fstipanov@gmail.com>
2022-01-25 14:42:57 +0100

    +

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-const container = document.querySelector('.container');
-const boxLeft = document.querySelector('.box-left');
-const boxRight = document.querySelector('.box-right');
+const loadItems = () => {
+  const container = document.querySelector('.container');
+  const boxLeft = document.querySelector('.box-left');
+  const boxRight = document.querySelector('.box-right');
 
-const addHoverLeft = () => {
-  container.classList.add('hover-left');
+  const addHoverLeft = () => {
+    container.classList.add('hover-left');
+  };
+
+  const removeHoverLeft = () => {
+    container.classList.remove('hover-left');
+  };
+
+  const addHoverRight = () => {
+    container.classList.add('hover-right');
+  };
+
+  const removeHoverRight = () => {
+    container.classList.remove('hover-right');
+  };
+
+  boxLeft.addEventListener('mouseenter', addHoverLeft);
+  boxRight.addEventListener('mouseenter', addHoverRight);
+  boxLeft.addEventListener('mouseleave', removeHoverLeft);
+  boxRight.addEventListener('mouseleave', removeHoverRight);
 };
 
-const removeHoverLeft = () => {
-  container.classList.remove('hover-left');
-};
-
-const addHoverRight = () => {
-  container.classList.add('hover-right');
-};
-
-const removeHoverRight = () => {
-  container.classList.remove('hover-right');
-};
-
-boxLeft.addEventListener('mouseenter', addHoverLeft);
-boxRight.addEventListener('mouseenter', addHoverRight);
-boxLeft.addEventListener('mouseleave', removeHoverLeft);
-boxRight.addEventListener('mouseleave', removeHoverRight);
+window.addEventListener('load', loadItems);
