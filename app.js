76fe2855002af89a8a81c5ff9922eb2d687135f0
fstipo <fstipanov@gmail.com>
2022-01-25 14:34:05 +0100

    Javascript

---
+++ app.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container');
+const boxLeft = document.querySelector('.box-left');
+const boxRight = document.querySelector('.box-right');
+
+const addHoverLeft = () => {
+  container.classList.add('hover-left');
+};
+
+const removeHoverLeft = () => {
+  container.classList.remove('hover-left');
+};
+
+const addHoverRight = () => {
+  container.classList.add('hover-right');
+};
+
+const removeHoverRight = () => {
+  container.classList.remove('hover-right');
+};
+
+boxLeft.addEventListener('mouseenter', addHoverLeft);
+boxRight.addEventListener('mouseenter', addHoverRight);
+boxLeft.addEventListener('mouseleave', removeHoverLeft);
+boxRight.addEventListener('mouseleave', removeHoverRight);
